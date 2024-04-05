662986aa837bda51ed74b73b1c377d5dae2c79f8
eugeniomerloni <164801392+eugeniomerloni@users.noreply.github.com>
2024-04-05 16:54:20 +0000

    Geocoding part working

---
+++ umbrella.rb
@@ -0,0 +1,59 @@
+pp "Howdy"
+
+pp "Where are uou located?"
+
+ user_location = gets.chomp.gsub(" ", "%20")
+
+# user_location = "Chicago"
+
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=" + ENV.fetch("GMAPS_KEY")
+
+p maps_url
+
+require "http"
+
+resp = HTTP.get(maps_url)
+
+raw_resp = resp.to_s
+
+# pp raw_resp
+
+require "json"
+
+parsed_response = JSON.parse(raw_resp)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+pp latitude = loc.fetch("lat")
+pp longitude = loc.fetch("lng")
+
+=begin
+# Hidden variables
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+# Assemble the full URL string by adding the first part, the API token, and the last part together
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+
+# Place a GET request to the URL
+raw_response = HTTP.get(pirate_weather_url)
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+currently_hash = parsed_response.fetch("currently")
+
+current_temp = currently_hash.fetch("temperature")
+
+puts "The current temperature is " + current_temp.to_s + "."
+=end
